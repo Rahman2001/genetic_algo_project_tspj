fa9e8e2ddad7d9f5e6f337797f1e5840accd2f14
Rahman Rejepov <rahmanrejepov777@gmail.com>
2023-12-30 19:38:11 +0300

    domain definition

--- domain/job.py
+++ domain/job.py
@@ -1,4 +1,8 @@
 
 class Job:
     number = 0
-    timeToComplete = 0
+    time_to_complete = 0
+
+    def __init__(self, number, time_to_complete):
+        self.number = number
+        self.time_to_complete = time_to_complete
--- domain/vertex.py
+++ domain/vertex.py
@@ -3,5 +3,14 @@ from job import Job
 
 class Vertex:
     number = 0
-    neighbors = [][1][1]   # [Vertex][distance]
+    neighbors = []  # [Vertex]
+    distances = []  # [distance for each vertex] in neighbors[i]
     job = Job
+
+    def __init__(self, number):
+        self.number = number
+
+    def add_neighbor_distance(self, vertex, distance):
+        self.neighbors.append(vertex)
+        self.distances.append(distance)
+
