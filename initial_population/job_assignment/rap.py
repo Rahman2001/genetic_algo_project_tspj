f15c80f23ad1a3f82c68fe7e76d5f1f4d96baecb
Rahman Rejepov <rahmanrejepov777@gmail.com>
2024-01-11 01:40:13 +0300

    random number generation improvement

--- initial_population/job_assignment/rap.py
+++ initial_population/job_assignment/rap.py
@@ -1,16 +1,13 @@
-from random import randint
+import random
 # Random Assignment Permutation (RAP)
 # Randomly assigns jobs by constructing an array of jobs
 
 
 def rap(jobs):
     rand_job = []
+    rand_index = random.sample(range(0, len(jobs)-1), len(jobs)-1)
 
-    while rand_job.__len__() != len(jobs):
-        rand_index = randint(0, len(jobs) - 1)
-        next_job = jobs[rand_index]
-        if not next_job.is_completed:
-            rand_job.append(jobs[rand_index].number)
-            next_job.set_job_completed(True)
+    for index in rand_index:
+        rand_job.append(jobs[index])
 
     return rand_job
