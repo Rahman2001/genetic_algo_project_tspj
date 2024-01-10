ee8babd524d7f74c623d59f124b78b35440fc23e
Rahman Rejepov <rahmanrejepov777@gmail.com>
2024-01-10 13:58:01 +0300

    NNJA algorithm construction done

---
+++ initial_population/job_assignment/nnja.py
@@ -0,0 +1,24 @@
+
+
+def nnja(route_path, jobs):
+
+    # job number must be equal to the vertices number to be visited
+    if len(jobs) == len(route_path) - 2:
+        print("Passed the IF statement!\n")
+        # sort jobs with the highest completion time in decreasing order
+        for i in range(len(jobs)):
+            job = jobs[i]
+            for j in range(i+1, len(jobs)):
+                if job.time_to_complete < jobs[j].time_to_complete:
+                    temp = jobs[i]
+                    jobs[i] = jobs[j]
+                    jobs[j] = temp
+
+                    job = jobs[i]
+
+            print("The job number " + str(job.number) + " has the highest completion time " + str(job.time_to_complete))
+
+        return jobs
+
+    print("Could not pass the IF statement!\n")
+    return None
--- main.py
+++ main.py
@@ -5,23 +5,25 @@ from domain.vertex import Vertex
 from initial_population.route_construction.nearest_neighbor import nearest_neighbor
 from initial_population.route_construction.random_route import random_route
 from initial_population.job_assignment.rap import rap
+from initial_population.job_assignment.nnja import nnja
 
 
 def main():
-    # v1 = Vertex(2)
-    # v2 = Vertex(4)
-    # v3 = Vertex(11)
-    # v4 = Vertex(43)
-    # v5 = Vertex(23)
-    #
-    # v_list = [v1, v2, v3, v4, v5]
-    # for i in range(v_list.__len__()):
-    #     for j in range(v_list.__len__()):
-    #         if i != j:
-    #             v_list[i].add_neighbor_distance(v_list[j], randint(1, 40))
+    v1 = Vertex(2)
+    v2 = Vertex(4)
+    v3 = Vertex(11)
+    v4 = Vertex(43)
+    v5 = Vertex(23)
+    v6 = Vertex(13)
 
-    # nn = nearest_neighbor(v_list)
-    # print(nn)
+    v_list = [v1, v2, v3, v4, v5, v6]
+    for i in range(v_list.__len__()):
+        for j in range(v_list.__len__()):
+            if i != j:
+                v_list[i].add_neighbor_distance(v_list[j], randint(1, 40))
+
+    nn = nearest_neighbor(v_list)
+    print(nn)
     # rr = random_route(v_list)
     # print(rr)
 
@@ -33,8 +35,11 @@ def main():
 
     j_list = [j1, j2, j3, j4, j5]
 
-    rand_job = rap(j_list)
-    print(rand_job)
+    # rand_job = rap(j_list)
+    # print(rand_job)
+
+    nnja_job = nnja(nn, j_list)
+    print([job.number for job in nnja_job])
 
 
 main()
