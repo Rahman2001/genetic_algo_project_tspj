3a2a5b7d62e5be4bf13b1486ae5b866b019fbdab
Rahman Rejepov <rahmanrejepov777@gmail.com>
2024-01-13 22:03:10 +0300

    RM algo correction

--- mutation/diversification/rm.py
+++ mutation/diversification/rm.py
@@ -3,20 +3,13 @@ import random
 
 def reverse_mutation(individual):
     # we chose sample length of job because we don't want to include depots in the route array
-    rand_int = random.sample((0, len(individual[1])-1), 2)
+    rand_int = random.sample((1, len(individual[1]) - 2), 2)
     print("rand_int: " + str([i for i in rand_int]))
 
     # reverse items in selected range
-    for i in range(len(individual)):
-        range_of_individual = [individual[i][j] for j in range(0, len(individual[i])) if rand_int[0] <= j <= rand_int[1]]
-        # we don't want to include depots in the route array of individual
-        if i == 0:
-            range_of_individual = [individual[i][j+1] for j in range(0, len(individual[i])) if rand_int[0] <= j <= rand_int[1]]
-        for j in range(rand_int[0], rand_int[1] + 1):
-            index = j
-            # wa want to reverse sub-tours by excluding depots in the route array
-            if i == 0:
-                index = index + 1
-            individual[i][index] = range_of_individual.pop()
+    range_of_individual = [individual[0][j] for j in range(0, len(individual[0])) if rand_int[0] <= j <= rand_int[1]]
+
+    for j in range(rand_int[0], rand_int[1]):
+        individual[0][j] = range_of_individual.pop()
 
     return individual
