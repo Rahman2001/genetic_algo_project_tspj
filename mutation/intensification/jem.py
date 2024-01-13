b7cebf72ee766e705e8776dcad34ca8a8af98bea
Rahman Rejepov <rahmanrejepov777@gmail.com>
2024-01-13 20:57:06 +0300

    JEM algorithm construction

---
+++ mutation/intensification/jem.py
@@ -0,0 +1,14 @@
+import random
+
+
+def job_exchange_mutation(individual):
+    jobs = individual[1]
+    # generate two random indices to exchange in jobs
+    rand_ind = random.sample((0, len(jobs) - 1), 2)
+    # swap operation
+    temp = jobs[rand_ind[0]]
+    jobs[rand_ind[0]] = jobs[rand_ind[1]]
+    jobs[rand_ind[1]] = temp
+
+    individual[1] = jobs
+    return jobs
